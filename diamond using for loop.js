9eb83c62db5b69dba01f972a1e5e00047d5fff5f
Anna-Tatinyan <31699506+Anna-Tatinyan@users.noreply.github.com>
2018-01-03 15:44:11 +0400

    Update diamond using for loop.js

--- diamond using for loop.js
+++ diamond using for loop.js
@@ -1,32 +1,23 @@
 const diamond = function(n, char) {
-
-
     const stars = function(num, character) {
-
         let str = '';
-
         for (let i = 0; i < num; i++) {
-
             str = str + character;
-
         }
-        return str
-
+        return str;
     };
 
     const spaces = function(num2) {
         let str = "";
         for (let i = 0; i < num2; i++) {
             str = str + "_";
-
         }
-        return str
+        return str;
     };
 
     const triangle = function(a, b, c) {
         for (; b > -1; a = a + 2, b = b - 1) {
             console.log(spaces(b) + stars(a, c))
-
         }
     };
     triangle(1, n / 2 - 1, char);
@@ -34,7 +25,6 @@ const diamond = function(n, char) {
     const reversetriangle = function(a, b, c) {
         for (; a > 0; a = a - 2, b = b + 1) {
             console.log(spaces(b) + stars(a, c))
-
         }
     };
     if (n % 2 === 0) {
@@ -42,5 +32,5 @@ const diamond = function(n, char) {
     } else {
         reversetriangle(n - 2, 1, char);
     }
-}
+};
 diamond();
